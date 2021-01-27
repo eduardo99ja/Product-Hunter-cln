94d9fefa1e21946d91b9e7c9cd42ad6e6763f400
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-01-27 14:55:31 -0600

    agregando normalize y las google fonts

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,8 +1,25 @@
 import Link from 'next/link'
 import Header from './Header'
+import Head from 'next/head'
 const Layout = props => {
   return (
     <>
+      <Head>
+        <html lang='es' />
+        <title>Produc hunt- Firebase</title>
+        <link
+          rel='stylesheet'
+          href='https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css'
+          integrity='sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=='
+          crossorigin='anonymous'
+        />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link
+          href='https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap'
+          rel='stylesheet'
+        />
+        <link href='/static/css/app.css' rel='stylesheet' />
+      </Head>
       <Header />
 
       <main>{props.children}</main>
