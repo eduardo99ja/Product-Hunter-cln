82848d74e58aee5024691c2745edcac99590a63b
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-01-28 13:19:43 -0600

    validando formulario crear cuenta y mostrando errores

--- pages/crear-cuenta.jsx
+++ pages/crear-cuenta.jsx
@@ -1,12 +1,31 @@
 import Layout from '../components/layout/Layout'
-
+//validacaiones
+import useValidation from '../hooks/useValidation'
+import validarCrearCuenta from '../validacion/validarCrearCuenta'
+const STATE_INICIAL = {
+  nombre: '',
+  email: '',
+  password: '',
+}
 export default function CrearCuenta() {
+  const {
+    valores,
+    errores,
+    handleSubmit,
+    handleChange,
+    handleBlur,
+  } = useValidation(STATE_INICIAL, validarCrearCuenta, crearCuenta)
+
+  const { nombre, email, password } = valores
+  function crearCuenta() {
+    console.log('creando cuenta')
+  }
   return (
     <div>
       <Layout>
         <>
           <h1 className='title_crear_cuenta'>Crear cuenta</h1>
-          <form className='formulario'>
+          <form className='formulario' onSubmit={handleSubmit} noValidate>
             <div className='campo'>
               <label htmlFor='nombre'>Nombre</label>
               <input
@@ -14,8 +33,12 @@ export default function CrearCuenta() {
                 id='nombre'
                 placeholder='Tu nombre'
                 name='nombre'
+                value={nombre}
+                onChange={handleChange}
+                onBlur={handleBlur}
               />
             </div>
+            {errores.nombre && <p className='error'>{errores.nombre}</p>}
             <div className='campo'>
               <label htmlFor='email'>Email</label>
               <input
@@ -23,8 +46,12 @@ export default function CrearCuenta() {
                 id='email'
                 placeholder='Tu email'
                 name='email'
+                value={email}
+                onChange={handleChange}
+                onBlur={handleBlur}
               />
             </div>
+            {errores.email && <p className='error'>{errores.email}</p>}
             <div className='campo'>
               <label htmlFor='password'>Password</label>
               <input
@@ -32,8 +59,12 @@ export default function CrearCuenta() {
                 id='password'
                 placeholder='Tu password'
                 name='password'
+                value={password}
+                onChange={handleChange}
+                onBlur={handleBlur}
               />
             </div>
+            {errores.password && <p className='error'>{errores.password}</p>}
             <input type='submit' value='Crear cuenta' className='inputSubmit' />
           </form>
         </>
