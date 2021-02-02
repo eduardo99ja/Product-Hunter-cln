fad57d0789756a383d623d6d26f86cacc6d4a108
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-02-02 13:04:10 -0600

    validando que el producto exista

---
+++ components/layout/Error404.jsx
@@ -0,0 +1,9 @@
+const Error404 = () => {
+  const style = {
+    marginTop: '5rem',
+    textAlign: 'center',
+  }
+  return <h1 style={style}>Producto no existente</h1>
+}
+
+export default Error404
--- pages/productos/[id].jsx
+++ pages/productos/[id].jsx
@@ -1,24 +1,52 @@
-import React, { useEffect } from 'react';
-import {useRouter} from 'next/router'
+import React, { useContext, useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { FirebaseContext } from '../../firebase'
+import Error404 from '../../components/layout/Error404'
+import Layout from '../../components/layout/Layout'
 
 const Producto = () => {
+  //state del componente
 
-    //Routing para obtener el id actual
+  const [producto, guardarProducto] = useState({})
+  const [error, guardarError] = useState(false)
+  const [comentario, guardarComentario] = useState({})
+  const [consultarDB, guardarConsultarDB] = useState(true)
 
-    const router = useRouter(
-        )
-    
-    const {query:{id}}= router
+  //Routing para obtener el id actual
 
-    useEffect(() => {
-        if(id){
-            console.log('ya hay un id');
+  // Routing para obtener el id actual
+  const router = useRouter()
+  const {
+    query: { id },
+  } = router
+  // context de firebase
+  const { firebase, usuario } = useContext(FirebaseContext)
+
+  useEffect(() => {
+    if (id) {
+      const obtenerProducto = async () => {
+        const productoQuery = await firebase.db.collection('productos').doc(id)
+        const producto = await productoQuery.get()
+        if (producto.exists) {
+          guardarProducto(producto.data())
+          guardarConsultarDB(false)
+        } else {
+          guardarError(true)
+          guardarConsultarDB(false)
         }
-        
-    }, [id])
-    return ( 
-        <h1>Desde {id}</h1>
-     );
+      }
+      obtenerProducto()
+    }
+  }, [id])
+
+  return (
+    <Layout>
+      <>
+      {error && <Error404 />}
+      <h1>desde {id}</h1>
+      </>
+    </Layout>
+  )
 }
- 
-export default Producto;
+
+export default Producto
